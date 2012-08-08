e7792c96e4530a594c74a63eed9f38344cd04274
tripp <trippp@gmail.com>
2012-08-08 15:56:26 -0700

    one more

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,6 @@
 
+<center id="footer">c 2012, 16ths.  "You must stay drunk on writing so reality cannot destroy you." Ray Bradbury</center>
+
 <!--<script type="text/javascript" src="/assets/js/bootstrap-tooltip.js"></script>
 <script type="text/javascript" src="/assets/js/bootstrap-popover.js"></script>
 <script type="text/javascript" src="/assets/js/bootstrap-alert.js"></script>-->
